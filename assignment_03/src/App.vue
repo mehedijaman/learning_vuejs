f45c674b25873be9b0d44e728bd215f72ba9b514
Mehedi Jaman <mail4mjaman@gmail.com>
2023-07-17 19:58:04 +0600

    - Assignment 03 completed

---
+++ assignment_03/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Header from './components/Header.vue'
+import Hero from './components/Hero.vue'
+import Main from './components/Main.vue'
+import Footer from './components/Footer.vue'
+
+
+</script>
+
+<template>
+    <Header/>
+    <Hero/>
+    <Main/>
+    <Footer/>
+
+  
+</template>
+
+<style scoped>
+</style>
