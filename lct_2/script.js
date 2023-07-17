601ee6827117a4858ad4218424192f1609dc2a58
Mehedi Jaman <mail4mjaman@gmail.com>
2023-07-17 17:36:28 +0600

    - Some unused files deleted

---
+++ lct_2/script.js
@@ -0,0 +1,10 @@
+const person = {
+    name: 'John',
+    age: 30,
+    city: 'New York',
+    occupation: 'Software Engineer'
+  }
+
+  const {name, age} = person
+  const data = `My name is ${name} and I am ${age} years old`
+  console.log(data)
